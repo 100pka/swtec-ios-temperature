9f9f15342367e945dd7bfdbc4e93be7ba7cce612
skulagin <stepan.kulagin@swtecnn.com>
2021-03-23 12:58:52 +0300

    some code optimisations

--- Swtcenn-practice/ViewController.swift
+++ Swtcenn-practice/ViewController.swift
@@ -13,51 +13,102 @@ class ViewController: UIViewController {
     @IBOutlet weak var farenheitTextField: UITextField!
     @IBOutlet weak var kelvinTextField: UITextField!
     
+    enum GradeType: Int {
+        case celcium
+        case farenheit
+        case kelvin
+    }
+    
+    enum ConstValues {
+        static let kelvinDiff = 273.15
+        static let celciumToFarenheitMultiplier = 1.8
+        static let celciumToFarenheitDiff = 32.0
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        celciumTextField.tag = 0
+        farenheitTextField.tag = 1
+        kelvinTextField.tag = 2
     }
     
-    @IBAction func celciumTextFieldEditindDidChanged(_ sender: Any) {
-        let celcium = Double(celciumTextField.text ?? "nil")
-        if celcium != nil {
-            farenheitTextField.text = String(format: "%.2f", celciumToFarenheit(value: celcium!))
+    @IBAction func textFieldEditingdDidChanged(_ sender: UITextField) {
+        switch sender.tag {
+        case GradeType.celcium.rawValue:
+            guard let celciumText = celciumTextField.text,
+                  let celciumValue = Double(celciumText),
+                  let farenheitValue = convertGrade(from: .celcium, to: .farenheit, celciumValue),
+                  let kelvinValue = convertGrade(from: .celcium, to: .kelvin, celciumValue)
+            else {
+                farenheitTextField.text = ""
+                kelvinTextField.text = ""
+                return
+            }
+            farenheitTextField.text = String(format: "%.2f", farenheitValue)
+            kelvinTextField.text = String(format: "%.2f", kelvinValue)
             
-            kelvinTextField.text = String(format: "%.2f", celcium! + 273.15)
-        } else {
-            farenheitTextField.text = ""
-            kelvinTextField.text = ""
-        }
-    }
-    
-    @IBAction func farenheitTextFieldEditindDidChanged(_ sender: Any) {
-        let farenheit = Double(farenheitTextField.text ?? "nil")
-        if farenheit != nil {
-            celciumTextField.text = String(format: "%.2f", farenheitToCelcium(value: farenheit!))
+        case GradeType.farenheit.rawValue:
+            guard let farenheitText = farenheitTextField.text,
+                  let farenheitValue = Double(farenheitText),
+                  let celciumValue = convertGrade(from: .farenheit, to: .celcium, farenheitValue),
+                  let kelvinValue = convertGrade(from: .celcium, to: .kelvin, celciumValue)
+            else {
+                celciumTextField.text = ""
+                kelvinTextField.text = ""
+                return
+            }
+            celciumTextField.text = String(format: "%.2f", celciumValue)
+            kelvinTextField.text = String(format: "%.2f", kelvinValue)
             
-            kelvinTextField.text = String(format: "%.2f", farenheitToCelcium(value: farenheit!) + 273.15)
-        } else {
-            celciumTextField.text = ""
-            kelvinTextField.text = ""
-        }
-    }
-    @IBAction func kelvinTextFieldEditindDidChanged(_ sender: Any) {
-        let kelvin = Double(kelvinTextField.text ?? "nil")
-        if kelvin != nil {
-            celciumTextField.text = String(format: "%.2f", kelvin! - 273.15)
-            farenheitTextField.text = String(format: "%.2f", celciumToFarenheit(value: kelvin! - 273.15))
-        } else {
-            celciumTextField.text = ""
-            farenheitTextField.text = ""
+        case GradeType.kelvin.rawValue:
+            guard let kelvinText = kelvinTextField.text,
+                  let kelvinValue = Double(kelvinText),
+                  let celciumValue = convertGrade(from: .kelvin, to: .celcium, kelvinValue),
+                  let farenheitValue = convertGrade(from: .celcium, to: .farenheit, celciumValue)
+            else {
+                farenheitTextField.text = ""
+                celciumTextField.text = ""
+                return
+            }
+            farenheitTextField.text = String(format: "%.2f", farenheitValue)
+            celciumTextField.text = String(format: "%.2f", celciumValue)
+        default:
+            return
         }
     }
     
-    func celciumToFarenheit(value celcium: Double) -> Double {
-        return celcium * 1.8 + 32
+    private func convertGrade(from initialType: GradeType, to requiredType: GradeType, _ value: Double) -> Double? {
+        let convertDirection = (initialType, requiredType)
+        switch convertDirection {
+        case (.celcium, .farenheit):
+            return celciumToFarenheit(value: value)
+        case (.farenheit, .celcium):
+            return farenheitToCelcium(value: value)
+        case (.kelvin, .celcium):
+            return kelvinToCelcium(value: value)
+        case (.celcium, .kelvin):
+            return celciumToKelvin(value: value)
+        default:
+            return nil
+        }
     }
     
-    func farenheitToCelcium(value farenheit: Double) -> Double {
-        return (farenheit - 32) / 1.8
+    private func celciumToFarenheit(value celcium: Double) -> Double {
+        return celcium * ConstValues.celciumToFarenheitMultiplier + ConstValues.celciumToFarenheitDiff
+    }
+    
+    private func farenheitToCelcium(value farenheit: Double) -> Double {
+        return (farenheit - ConstValues.celciumToFarenheitDiff) / ConstValues.celciumToFarenheitMultiplier
+    }
+    
+    private func kelvinToCelcium(value kelvin: Double) -> Double {
+        return kelvin - ConstValues.kelvinDiff
+    }
+    
+    private func celciumToKelvin(value celcium: Double) -> Double {
+        return celcium + ConstValues.kelvinDiff
     }
 }
 
